3c40f6839ba7c08a3cbb882accbecec80b8ab29a
Andhika <andhikay24@gmail.com>
2020-10-08 17:22:07 +0700

    template

---
+++ resources/views/template/base/app.blade.php
@@ -0,0 +1,251 @@
+<!doctype html>
+<html lang="en" class="no-focus">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+        <title>USP-BKS Jawa Timur 2020 | @yield('title')</title>
+
+        <meta name="description" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta name="author" content="pixelcave">
+        <meta name="robots" content="noindex, nofollow">
+
+        <!-- Open Graph Meta -->
+        <meta property="og:title" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework">
+        <meta property="og:site_name" content="Codebase">
+        <meta property="og:description" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="">
+        <meta property="og:image" content="">
+
+        <!-- Icons -->
+        <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+        <link rel="shortcut icon" href="assets/media/favicons/favicon.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="assets/media/favicons/favicon-192x192.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/media/favicons/apple-touch-icon-180x180.png">
+        <!-- END Icons -->
+
+        <!-- Stylesheets -->
+
+        <!-- Fonts and Codebase framework -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,400i,600,700">
+        <link rel="stylesheet" id="css-main" href="{{ asset('codebase/src/assets/css/codebase.min.css') }}">
+
+        <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
+        <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/flat.min.css"> -->
+        <!-- END Stylesheets -->
+    </head>
+    <body>
+
+        <!-- Page Container -->
+        <!--
+            Available classes for #page-container:
+
+        GENERIC
+
+            'enable-cookies'                            Remembers active color theme between pages (when set through color theme helper Template._uiHandleTheme())
+
+        SIDEBAR & SIDE OVERLAY
+
+            'sidebar-r'                                 Right Sidebar and left Side Overlay (default is left Sidebar and right Side Overlay)
+            'sidebar-mini'                              Mini hoverable Sidebar (screen width > 991px)
+            'sidebar-o'                                 Visible Sidebar by default (screen width > 991px)
+            'sidebar-o-xs'                              Visible Sidebar by default (screen width < 992px)
+            'sidebar-inverse'                           Dark themed sidebar
+
+            'side-overlay-hover'                        Hoverable Side Overlay (screen width > 991px)
+            'side-overlay-o'                            Visible Side Overlay by default
+
+            'enable-page-overlay'                       Enables a visible clickable Page Overlay (closes Side Overlay on click) when Side Overlay opens
+
+            'side-scroll'                               Enables custom scrolling on Sidebar and Side Overlay instead of native scrolling (screen width > 991px)
+
+        HEADER
+
+            ''                                          Static Header if no class is added
+            'page-header-fixed'                         Fixed Header
+
+        HEADER STYLE
+
+            ''                                          Classic Header style if no class is added
+            'page-header-modern'                        Modern Header style
+            'page-header-inverse'                       Dark themed Header (works only with classic Header style)
+            'page-header-glass'                         Light themed Header with transparency by default
+                                                        (absolute position, perfect for light images underneath - solid light background on scroll if the Header is also set as fixed)
+            'page-header-glass page-header-inverse'     Dark themed Header with transparency by default
+                                                        (absolute position, perfect for dark images underneath - solid dark background on scroll if the Header is also set as fixed)
+
+        MAIN CONTENT LAYOUT
+
+            ''                                          Full width Main Content if no class is added
+            'main-content-boxed'                        Full width Main Content with a specific maximum width (screen width > 1200px)
+            'main-content-narrow'                       Full width Main Content with a percentage width (screen width > 1200px)
+        -->
+        <div id="page-container" class="sidebar-o sidebar-inverse enable-page-overlay side-scroll page-header-fixed page-header-modern main-content-boxed">
+            <!-- Side Overlay-->
+            <aside id="side-overlay">
+                <!-- Side Header -->
+                <div class="content-header content-header-fullrow">
+                    <div class="content-header-section align-parent">
+                        <!-- Close Side Overlay -->
+                        <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                        <button type="button" class="btn btn-circle btn-dual-secondary align-v-r" data-toggle="layout" data-action="side_overlay_close">
+                            <i class="fa fa-times text-danger"></i>
+                        </button>
+                        <!-- END Close Side Overlay -->
+
+                        <!-- User Info -->
+                        <div class="content-header-item">
+                            <a class="img-link mr-5" href="be_pages_generic_profile.html">
+                                <img class="img-avatar img-avatar32" src="assets/media/avatars/avatar15.jpg" alt="">
+                            </a>
+                            <a class="align-middle link-effect text-primary-dark font-w600" href="be_pages_generic_profile.html">John Smith</a>
+                        </div>
+                        <!-- END User Info -->
+                    </div>
+                </div>
+                <!-- END Side Header -->
+
+                <!-- Side Content -->
+                <div class="content-side">
+                    <!-- Mini Stats -->
+                    <div class="block pull-r-l">
+                        <div class="block-content block-content-full block-content-sm bg-body-light">
+                            <div class="row">
+                                <div class="col-4">
+                                    <div class="font-size-sm font-w600 text-uppercase text-muted">Clients</div>
+                                    <div class="font-size-h4">460</div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="font-size-sm font-w600 text-uppercase text-muted">Sales</div>
+                                    <div class="font-size-h4">728</div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="font-size-sm font-w600 text-uppercase text-muted">Earnings</div>
+                                    <div class="font-size-h4">$7,860</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- END Mini Stats -->
+
+                    <!-- Profile -->
+                    <div class="block pull-r-l">
+                        <div class="block-header bg-body-light">
+                            <h3 class="block-title">
+                                <i class="fa fa-fw fa-pencil font-size-default mr-5"></i>Profile
+                            </h3>
+                            <div class="block-options">
+                                <button type="button" class="btn-block-option" data-toggle="block-option" data-action="content_toggle"></button>
+                            </div>
+                        </div>
+                        <div class="block-content">
+                            <form action="be_pages_dashboard.html" method="post" onsubmit="return false;">
+                                <div class="form-group mb-15">
+                                    <label for="side-overlay-profile-name">Name</label>
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" id="side-overlay-profile-name" name="side-overlay-profile-name" placeholder="Your name.." value="John Smith">
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">
+                                                <i class="fa fa-user"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group mb-15">
+                                    <label for="side-overlay-profile-email">Email</label>
+                                    <div class="input-group">
+                                        <input type="email" class="form-control" id="side-overlay-profile-email" name="side-overlay-profile-email" placeholder="Your email.." value="john.smith@example.com">
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">
+                                                <i class="fa fa-envelope"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group mb-15">
+                                    <label for="side-overlay-profile-password">New Password</label>
+                                    <div class="input-group">
+                                        <input type="password" class="form-control" id="side-overlay-profile-password" name="side-overlay-profile-password" placeholder="New Password..">
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">
+                                                <i class="fa fa-asterisk"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group mb-15">
+                                    <label for="side-overlay-profile-password-confirm">Confirm New Password</label>
+                                    <div class="input-group">
+                                        <input type="password" class="form-control" id="side-overlay-profile-password-confirm" name="side-overlay-profile-password-confirm" placeholder="Confirm New Password..">
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">
+                                                <i class="fa fa-asterisk"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-6">
+                                        <button type="submit" class="btn btn-block btn-alt-primary">
+                                            <i class="fa fa-refresh mr-5"></i> Update
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- END Profile -->
+                </div>
+                <!-- END Side Content -->
+            </aside>
+            <!-- END Side Overlay -->
+
+            @yield('sidebar')
+            @yield('header')
+
+            <!-- Main Container -->
+            <main id="main-container">
+
+                <!-- Page Content -->
+                <div class="content">
+                    @yield('content')
+                </div>
+                <!-- END Page Content -->
+
+            </main>
+            <!-- END Main Container -->
+            @include('template.base.footer')
+        </div>
+        <!-- END Page Container -->
+        <!--
+            Codebase JS Core
+
+            Vital libraries and plugins used in all pages. You can choose to not include this file if you would like
+            to handle those dependencies through webpack. Please check out assets/_es6/main/bootstrap.js for more info.
+
+            If you like, you could also include them separately directly from the assets/js/core folder in the following
+            order. That can come in handy if you would like to include a few of them (eg jQuery) from a CDN.
+
+            assets/js/core/jquery.min.js
+            assets/js/core/bootstrap.bundle.min.js
+            assets/js/core/simplebar.min.js
+            assets/js/core/jquery-scrollLock.min.js
+            assets/js/core/jquery.appear.min.js
+            assets/js/core/jquery.countTo.min.js
+            assets/js/core/js.cookie.min.js
+        -->
+        <script src="{{ asset('codebase/src/assets/js/codebase.core.min.js')}}"></script>
+        <!--
+            Codebase JS
+            Custom functionality including Blocks/Layout API as well as other vital and optional helpers
+            webpack is putting everything together at assets/_es6/main/app.js
+        -->
+        <script src="{{ asset('codebase/src/assets/js/codebase.app.min.js')}}"></script>
+        <!-- Page JS Plugins -->
+        <script src="{{ asset('codebase/src/assets/js/plugins/chartjs/Chart.bundle.min.js')}}"></script>
+        <!-- Page JS Code -->
+        <script src="{{ asset('codebase/src/assets/js/pages/be_pages_dashboard.min.js')}}"></script>
+        @yield('moreJS')
+    </body>
+</html>
---
+++ resources/views/template/base/sidebar.blade.php
@@ -0,0 +1,254 @@
+<!-- Sidebar -->
+<!--
+    Helper classes
+
+    Adding .sidebar-mini-hide to an element will make it invisible (opacity: 0) when the sidebar is in mini mode
+    Adding .sidebar-mini-show to an element will make it visible (opacity: 1) when the sidebar is in mini mode
+        If you would like to disable the transition, just add the .sidebar-mini-notrans along with one of the previous 2 classes
+
+    Adding .sidebar-mini-hidden to an element will hide it when the sidebar is in mini mode
+    Adding .sidebar-mini-visible to an element will show it only when the sidebar is in mini mode
+        - use .sidebar-mini-visible-b if you would like to be a block when visible (display: block)
+-->
+<nav id="sidebar">
+    <!-- Sidebar Content -->
+    <div class="sidebar-content">
+        <!-- Side Header -->
+        <div class="content-header content-header-fullrow px-15">
+            <!-- Mini Mode -->
+            <div class="content-header-section sidebar-mini-visible-b">
+                <!-- Logo -->
+                <span class="content-header-item font-w700 font-size-xl float-left animated fadeIn">
+                    <span class="text-dual-primary-dark">c</span><span class="text-primary">b</span>
+                </span>
+                <!-- END Logo -->
+            </div>
+            <!-- END Mini Mode -->
+
+            <!-- Normal Mode -->
+            <div class="content-header-section text-center align-parent sidebar-mini-hidden">
+                <!-- Close Sidebar, Visible only on mobile screens -->
+                <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                <button type="button" class="btn btn-circle btn-dual-secondary d-lg-none align-v-r" data-toggle="layout" data-action="sidebar_close">
+                    <i class="fa fa-times text-danger"></i>
+                </button>
+                <!-- END Close Sidebar -->
+
+                <!-- Logo -->
+                <div class="content-header-item">
+                    <a class="link-effect font-w700" href="index.html">
+                        <i class="si si-fire text-primary"></i>
+                        <span class="font-size-xl text-dual-primary-dark">code</span><span class="font-size-xl text-primary">base</span>
+                    </a>
+                </div>
+                <!-- END Logo -->
+            </div>
+            <!-- END Normal Mode -->
+        </div>
+        <!-- END Side Header -->
+
+        <!-- Side User -->
+        <div class="content-side content-side-full content-side-user px-10 align-parent">
+            <!-- Visible only in mini mode -->
+            <div class="sidebar-mini-visible-b align-v animated fadeIn">
+                <img class="img-avatar img-avatar32" src="assets/media/avatars/avatar15.jpg" alt="">
+            </div>
+            <!-- END Visible only in mini mode -->
+
+            <!-- Visible only in normal mode -->
+            <div class="sidebar-mini-hidden-b text-center">
+                <a class="img-link" href="be_pages_generic_profile.html">
+                    <img class="img-avatar" src="assets/media/avatars/avatar15.jpg" alt="">
+                </a>
+                <ul class="list-inline mt-10">
+                    <li class="list-inline-item">
+                        <a class="link-effect text-dual-primary-dark font-size-xs font-w600 text-uppercase" href="be_pages_generic_profile.html">J. Smith</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                        <a class="link-effect text-dual-primary-dark" data-toggle="layout" data-action="sidebar_style_inverse_toggle" href="javascript:void(0)">
+                            <i class="si si-drop"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="link-effect text-dual-primary-dark" href="op_auth_signin.html">
+                            <i class="si si-logout"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- END Visible only in normal mode -->
+        </div>
+        <!-- END Side User -->
+
+        <!-- Side Navigation -->
+        <div class="content-side content-side-full">
+            <ul class="nav-main">
+                <li>
+                    <a href="be_pages_dashboard.html"><i class="si si-cup"></i><span class="sidebar-mini-hide">Dashboard</span></a>
+                </li>
+                <li class="open">
+                    <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-badge"></i><span class="sidebar-mini-hide">Page Kits</span></a>
+                    <ul>
+                        <li class="open">
+                            <a class="nav-submenu" data-toggle="nav-submenu" href="#"><span class="sidebar-mini-hide">Dashboards</span></a>
+                            <ul>
+                                <li>
+                                    <a href="be_pages_dashboard2.html"><span class="sidebar-mini-hide">Dashboard 2</span></a>
+                                </li>
+                                <li>
+                                    <a class="active" href="be_pages_dashboard3.html"><span class="sidebar-mini-hide">Dashboard 3</span></a>
+                                </li>
+                                <li>
+                                    <a href="be_pages_dashboard4.html"><span class="sidebar-mini-hide">Dashboard 4</span></a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="nav-submenu" data-toggle="nav-submenu" href="#"><span class="sidebar-mini-hide">Real Estate</span></a>
+                            <ul>
+                                <li>
+                                    <a href="be_pages_real_estate_dashboard.html">Dashboard</a>
+                                </li>
+                                <li>
+                                    <a href="be_pages_real_estate_listing.html">Listing</a>
+                                </li>
+                                <li>
+                                    <a href="be_pages_real_estate_listing_new.html">Add New Listing</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-main-heading"><span class="sidebar-mini-visible">UI</span><span class="sidebar-mini-hidden">User Interface</span></li>
+                <li>
+                    <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-puzzle"></i><span class="sidebar-mini-hide">Blocks</span></a>
+                    <ul>
+                        <li>
+                            <a href="be_blocks.html">Styles</a>
+                        </li>
+                        <li>
+                            <a href="be_blocks_tiles.html">Tiles</a>
+                        </li>
+                        <li>
+                            <a href="be_blocks_draggable.html">Draggable</a>
+                        </li>
+                        <li>
+                            <a href="be_blocks_api.html">API</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-moustache"></i><span class="sidebar-mini-hide">Widgets</span></a>
+                    <ul>
+                        <li>
+                            <a href="be_blocks_widgets_users.html">Users</a>
+                        </li>
+                        <li>
+                            <a href="be_blocks_widgets_stats.html">Statistics</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-energy"></i><span class="sidebar-mini-hide">Elements</span></a>
+                    <ul>
+                        <li>
+                            <a href="be_ui_grid.html">Grid</a>
+                        </li>
+                        <li>
+                            <a href="be_ui_icons.html">Icons</a>
+                        </li>
+                        <li>
+                            <a href="be_ui_typography.html">Typography</a>
+                        </li>
+                        <li>
+                            <a href="be_ui_activity.html">Activity</a>
+                        </li>
+                        <li>
+                            <a href="be_ui_buttons.html">Buttons</a>
+                        </li>
+                        <li>
+                            <a href="be_ui_navigation.html">Navigation</a>
+                        </li>
+                        <li>
+                            <a href="be_ui_tabs.html">Tabs</a>
+                        </li>
+                        <li>
+                            <a href="be_ui_modals_tooltips.html">Modals &amp; Tooltips</a>
+                        </li>
+                        <li>
+                            <a href="be_ui_images.html">Images</a>
+                        </li>
+                        <li>
+                            <a href="be_ui_animations.html">Animations</a>
+                        </li>
+                        <li>
+                            <a href="be_ui_ribbons.html">Ribbons</a>
+                        </li>
+                        <li>
+                            <a href="be_ui_timeline.html">Timeline</a>
+                        </li>
+                        <li>
+                            <a href="be_ui_accordion.html">Accordion</a>
+                        </li>
+                        <li>
+                            <a href="be_ui_color_themes.html">Color Themes</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-layers"></i><span class="sidebar-mini-hide">Tables</span></a>
+                    <ul>
+                        <li>
+                            <a href="be_tables_styles.html">Styles</a>
+                        </li>
+                        <li>
+                            <a href="be_tables_responsive.html">Responsive</a>
+                        </li>
+                        <li>
+                            <a href="be_tables_helpers.html">Helpers</a>
+                        </li>
+                        <li>
+                            <a href="be_tables_pricing.html">Pricing</a>
+                        </li>
+                        <li>
+                            <a href="be_tables_datatables.html">DataTables</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-note"></i><span class="sidebar-mini-hide">Forms</span></a>
+                    <ul>
+                        <li>
+                            <a href="be_forms_elements_bootstrap.html">Bootstrap Elements</a>
+                        </li>
+                        <li>
+                            <a href="be_forms_elements_material.html">Material Elements</a>
+                        </li>
+                        <li>
+                            <a href="be_forms_css_inputs.html">CSS Inputs</a>
+                        </li>
+                        <li>
+                            <a href="be_forms_plugins.html">Plugins</a>
+                        </li>
+                        <li>
+                            <a href="be_forms_editors.html">Editors</a>
+                        </li>
+                        <li>
+                            <a href="be_forms_validation.html">Validation</a>
+                        </li>
+                        <li>
+                            <a href="be_forms_wizard.html">Wizard</a>
+                        </li>
+                        <li>
+                            <a href="be_forms_premade.html">Pre-made</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- END Side Navigation -->
+    </div>
+    <!-- Sidebar Content -->
+</nav>
+<!-- END Sidebar -->

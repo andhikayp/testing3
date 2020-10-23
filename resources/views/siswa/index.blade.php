881992d6bcd577541c2d9a3daa0649cc4e481a52
Andhika <andhikay24@gmail.com>
2020-10-23 18:51:48 +0700

    bisa

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -10,6 +10,15 @@
 @endsection
 
 @section('content')
+<style>
+    td.details-control {
+        background: url('{{ asset('img/details_open.png') }}') no-repeat center center;
+        cursor: pointer;
+    }
+    tr.shown td.details-control {
+        background: url('{{ asset('img/details_close.png') }}') no-repeat center center;
+    }
+</style>
 <nav class="breadcrumb bg-white push">
     <a class="breadcrumb-item" href="{{ url('/dashboard') }}">Dashboard</a>
     <span class="breadcrumb-item active">Data Siswa</span>
@@ -23,6 +32,7 @@
             <table class="table table-bordered table-striped table-hover dataTable js-basic-example" id="users-table">
                 <thead>
                     <tr>
+                        <th></th>
                         <th>Kode Rayon</th>
                         <th>Kota / Kabupaten</th>
                     </tr>
@@ -35,8 +45,26 @@
 
 @section('moreJS')
     <script>
+        function format ( d ) {
+            // `d` is the original data object for the row
+            return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+                '<tr>'+
+                    '<td>Full name:</td>'+
+                    '<td>Andhika</td>'+
+                '</tr>'+
+                '<tr>'+
+                    '<td>Extension number:</td>'+
+                    '<td>Yoga</td>'+
+                '</tr>'+
+                '<tr>'+
+                    '<td>Extra info:</td>'+
+                    '<td>And any further details here (images etc)...</td>'+
+                '</tr>'+
+            '</table>';
+        }
+
         $(function(){
-            $('#users-table').DataTable({
+            var table = $('#users-table').DataTable({
                 "processing": true,
                 "serverSide": true,
                 "searchDelay": 1000,
@@ -47,12 +75,36 @@
                     "type": "GET",
                 },
                 "columns": [
+                    {
+                        "className":      'details-control',
+                        "orderable":      false,
+                        "data":           null,
+                        "defaultContent": ''
+                    },
                     { data: 'kd_rayon'},
                     { data: 'nama'},
                 ],
                 "order": [[1, "asc"]]
             });
+
+            // Add event listener for opening and closing details
+            $('#users-table tbody').on('click', 'td.details-control', function () {
+                var tr = $(this).closest('tr');
+                var row = table.row( tr );
+         
+                if ( row.child.isShown() ) {
+                    // This row is already open - close it
+                    row.child.hide();
+                    tr.removeClass('shown');
+                }
+                else {
+                    // Open this row
+                    row.child( format(row.data()) ).show();
+                    tr.addClass('shown');
+                }
+            } );
         });
+
     </script>
     <!-- Page JS Plugins -->
     <script src="{{ asset('codebase/src/assets/js/plugins/datatables/jquery.dataTables.min.js')}}"></script>
